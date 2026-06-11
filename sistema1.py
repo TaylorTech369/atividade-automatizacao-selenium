765989a81a56958a79f2e65a2aa8c4fab05e0f5f
TaylorTech369 <joaovictormendonca0411@gmail.com>
2026-06-11 16:35:57 -0300

    ativadade de automação com Selenium

---
+++ sistema1.py
@@ -0,0 +1,102 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+navegador = webdriver.Chrome()
+
+navegador.get("https://sistema-1-login.netlify.app ")
+
+navegador.maximize_window()
+
+time.sleep(0.5)
+
+# objetivo da automação do Sistema1:
+# criar conta,
+# fazer login, 
+# excluir as contas até restarem uma,
+# tirar printscreen.
+# fazer logout.
+
+navegador.find_element(By.ID, "goRegister").click()
+
+time.sleep(1)
+
+navegador.find_element(By.ID, "name").send_keys("João Victor")
+navegador.find_element(By.ID, "email").send_keys("Joao@gmail.com")
+navegador.find_element(By.ID, "password").send_keys("123456Ph")
+navegador.find_element(By.ID, "confirmPassword").send_keys("123456Ph")
+navegador.find_element(By.ID, "terms").click()
+
+time.sleep(1)
+
+navegador.find_element(By.ID, "createAccountButton").click()
+
+time.sleep(3)
+
+
+navegador.find_element(By.ID, "email").send_keys("Joao@gmail.com")
+navegador.find_element(By.ID, "password").send_keys("123456Ph")
+
+navegador.find_element(By.ID, "loginButton").click()
+time.sleep(3)
+
+# botoes = navegador.find_elements(By.CLASS_NAME, "Excluir").text
+
+
+
+
+
+
+
+
+
+
+botoes = navegador.find_elements(By.XPATH, "//button[contains(., 'Excluir')]")[0].click()
+navegador.switch_to.alert.accept()
+
+time.sleep(1)
+
+botoes = navegador.find_elements(By.XPATH, "//button[contains(., 'Excluir')]")[0].click()
+navegador.switch_to.alert.accept()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# navegador.find_elements(By.XPATH, "//button[contains(., ' Excluir ')]")[1].click()
+
+# botoes = navegador.find_elements(By.XPATH, "//*[text()='Excluir']")
+
+# navegador.find_element(By.ID, "botao").click()
+
+# mensagem = navegador.find_element(By.ID, "Excluir").text
+
+
+# linhas = navegador.find_elements(By.TAG_NAME, "tr")
+
+# <button class="btn delete" onclick="deleteUser(2)"> Excluir </button>
+
+
+
+navegador.save_screenshot("printScreenSistema1.png")
+
+navegador.find_element(By.ID, "logoutButton").click()
+
+time.sleep(5)
---
+++ sistema2.py
@@ -0,0 +1,81 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+navegador = webdriver.Chrome()
+
+navegador.get("https://sistema-2-loja.netlify.app ")
+
+navegador.maximize_window()
+
+time.sleep(2)
+
+# Criar Conta
+
+# id="name"
+# id="email"
+# id="password"
+# ---
+# id="depositInput"
+# id="depositButton"
+# ---
+# id="productsTabButton"
+# Adicionar ao Carrinho
+# (ok mensagem)
+# ---
+# Carrinho
+# class="checkout"
+# id="finishButton"
+# (printScreen)
+# (ok mensagem)
+
+navegador.find_element(By.XPATH, "//*[contains(text(), 'Criar Conta')]").click()
+
+time.sleep(1)
+
+navegador.find_element(By.ID, "name").send_keys("joão")
+navegador.find_element(By.ID, "email").send_keys("joao@gmail.com")
+navegador.find_element(By.ID, "password").send_keys("123456Ph")
+time.sleep(1)
+botoes = navegador.find_elements(By.XPATH, "//button[contains(., 'Cadastrar')]")[0].click()
+navegador.switch_to.alert.accept()
+
+time.sleep(1)
+
+navegador.find_element(By.ID, "email").send_keys("joao@gmail.com")
+navegador.find_element(By.ID, "password").send_keys("123456Ph")
+navegador.find_element(By.ID, "loginButton").click()
+time.sleep(1)
+
+navegador.find_element(By.ID, "depositInput").send_keys("100000000000000000000000000004500")
+navegador.find_element(By.ID, "depositButton").click()
+navegador.switch_to.alert.accept()
+
+time.sleep(1)
+
+navegador.find_element(By.ID, "productsTabButton").click()
+
+navegador.find_elements(By.XPATH, "//button[contains(., 'Adicionar ao Carrinho')]")[0].click()
+time.sleep(0.5)
+navegador.switch_to.alert.accept()
+
+time.sleep(1)
+
+navegador.find_element(By.XPATH, "//a[contains(text(), 'Carrinho')]").click()
+
+navegador.find_element(By.CLASS_NAME, "checkout").click()
+
+navegador.save_screenshot("printScreenSistema2-1.png")
+
+navegador.find_element(By.ID, "finishButton").click()
+navegador.switch_to.alert.accept()
+
+# time.sleep(1)
+
+# navegador.save_screenshot("printScreenSistema2-2.png")
+
+time.sleep(3)
+
+
+
+# navegador.save_screenshot("printScreenSistema2.png")
